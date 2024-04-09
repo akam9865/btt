825eb5c533dc8ec189adaaf6fc505abbb56b5e0c
akam <akam9865@gmail.com>
2024-04-08 22:00:20 -0700

    fix build

--- src/features/game/utils/utils.ts
+++ src/features/game/utils/utils.ts
@@ -1,7 +1,7 @@
-import { Position } from "@/stores/game";
+import { Move } from "@/utils/schema/MovesSchema";
 
 // Returns a formatted string of the position with human readable
 // indices for display and react key purposes only.
-export function joinPosition(position: Position): string {
+export function joinPosition(position: Move["position"]): string {
   return [position.bigBoardIndex + 1, position.littleBoardIndex + 1].join("-");
 }
